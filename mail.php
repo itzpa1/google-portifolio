f309bb9c3396d87b23d300b874467c6179ad6ce3
Pawan Kumar <65233567+itzpa1@users.noreply.github.com>
2024-02-21 13:55:34 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,11 +3,10 @@ if(isset($_POST['submit'])){
     $to = "pawankumarfz12@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
     $phone = $_POST['phone'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $first_name .  $phone . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
